139b1ce9e0c6d0ec2a569280607baf0a375b18c7
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-04 00:02:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,21 @@
 class Author 
-  attr_accessor :name 
+  attr_accessor :name
   def initialize(name)
     @name = name 
   end
   def posts 
     Post.all 
   end
+  def add_post(post_name)
+    Post.author = self
+  end
   
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+  
+  def self.song_count
+    Song.all.length 
+  end
 end
